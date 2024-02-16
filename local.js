98f32aba071838bd22e8ac388e88146b312c1531
kusumalathayele <kusumalathayele@gmail.com>
2024-02-16 12:47:39 +0530

    local1

---
+++ local.js
@@ -0,0 +1,14 @@
+let express=require("express")
+app=express()
+const {LocalStorage}=require("node-localstorage")
+
+app.use(express.json())
+const  localstorage= new LocalStorage("./localtask1")
+app.get("/users",(req,res)=>{
+    localstorage.setItem("value1",JSON.stringify(req.body))
+    const store=localstorage.getItem("value1")
+    res.json(JSON.parse(store))
+}).listen(3000,()=>{
+    console.log("port is runnig");
+})
+
